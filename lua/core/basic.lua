74ad306d3f06592c4548129005bf277e7cf4c432
FourteenD <2899385645@qq.com>
2022-09-18 02:51:43 +0800

    Changes to be committed: 	modified:   lua/core/basic.lua

--- lua/core/basic.lua
+++ lua/core/basic.lua
@@ -30,8 +30,6 @@ vim.o.smartindent = true
 -- 搜索大小写不敏感，除非包含大写
 vim.o.ignorecase = true
 vim.o.smartcase = true
--- 搜索不要高亮
-vim.o.hlsearch = false
 -- 边输入边搜索
 vim.o.incsearch = true
 -- 使用增强状态栏后不再需要 vim 的模式提示
@@ -41,9 +39,6 @@ vim.o.cmdheight = 2
 -- 当文件被外部程序修改时，自动加载
 vim.o.autoread = true
 vim.bo.autoread = true
--- 禁止折行
-vim.o.wrap = false
-vim.wo.wrap = false
 -- 行结尾可以跳到下一行
 vim.o.whichwrap = 'b,s,<,>,[,],h,l'
 -- 允许隐藏被修改过的buffer
